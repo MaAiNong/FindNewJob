50d509a2cffe1571c863e49ec87c5330d97e6815
sunuo <xianyirenxunuo@gmail.com>
2018-12-26 22:20:22 +0800

    Initial Commit

---
+++ FindJob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindJob
+//
+//  Created by kevin on 2018/12/26.
+//  Copyright © 2018 徐诺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindJob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindJob
+//
+//  Created by kevin on 2018/12/26.
+//  Copyright © 2018 徐诺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
